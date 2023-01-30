ae119dc732b9f5be146997319dac970748ce8b00
JeanBaptiste Allemandou <jeanbaptiste.allemandou@gmail.com>
2023-01-30 19:24:37 +0700

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # Add a logic to check if the user is a client or an expert and redirect to the right
+    # Add a delay and show a loader here
+    redirect_to client_path
   end
+
+  def dashboardclient
+
+  end
+
 end
