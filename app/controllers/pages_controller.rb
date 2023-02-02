03ec1375b226d92690353759d28b6a0a0ccf82b8
JeanBaptiste Allemandou <jeanbaptiste.allemandou@gmail.com>
2023-02-03 00:43:24 +0700

    sticky navbar , login and redirecting , logout functional with stimulus, general layout of the dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,8 @@ class PagesController < ApplicationController
   def home
     # Add a logic to check if the user is a client or an expert and redirect to the right
     # Add a delay and show a loader here
-    redirect_to client_path
+    redirect_to client_path unless current_user.nil?
+
   end
 
   def dashboardclient
